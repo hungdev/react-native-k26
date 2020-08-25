6070145020032ce46ac34262c4b1f4bbf353050a
Cee <cee@Cees-MacBook-Pro.local>
2020-08-25 22:13:01 +0700

    b1

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import stylezzzzdddasads from './appstyles'
+import { variableStyle } from './appstyles'
+import Login from './src/screens/LoginScreen'
+export default class App extends Component {
+  render() {
+    return (
+      <Login />
+      // <View style={{
+      //   // flexDirection: 'row',
+      //   // justifyContent: 'space-between',
+      //   // alignItems: 'center',
+      //   borderWidth: 5,
+      //   flex: 1
+      // }}>
+      //   <View style={{
+      //     borderWidth: 5, marginTop: 100,
+      //     height: 300, width: 200, position: 'relative'
+      //   }}>
+      //     <View style={{
+      //       position: 'absolute',
+      //       top: 10,
+      //       // marginTop: 5000,
+      //       height: 50, width: 50,
+      //       backgroundColor: 'red'
+      //     }}></View>
+      //   </View>
+      //   {/* <View style={{ flex: 2, height: 50, width: 50, backgroundColor: 'blue' }}></View>
+      //   <View style={{ flex: 1, height: 50, width: 50, backgroundColor: 'grey' }}></View> */}
+      // </View>
+    )
+  }
+}
+
+
---
+++ appstyles.js
@@ -0,0 +1,8 @@
+import { Text, View, StyleSheet } from 'react-native'
+const styles = StyleSheet.create({
+  abc: {
+    backgroundColor: 'green',
+  },
+});
+export const variableStyle = '1111'
+export default styles
