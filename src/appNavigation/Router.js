ae82e1231144953be94fb4fd4b48c9a2f101148a
Cee <cee@Cees-MacBook-Pro.local>
2020-09-03 21:00:33 +0700

    add switch

--- src/appNavigation/Router.js
+++ src/appNavigation/Router.js
@@ -2,24 +2,57 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen'
 import LoginScreen from '../screens/LoginScreen'
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-function MyTabs() {
+function TabHome() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Settings" component={LoginScreen} />
+      {/* <Tab.Screen name="Settings" component={LoginScreen} /> */}
       <Tab.Screen name="Home" component={HomeScreen} />
     </Tab.Navigator>
+  );
+}
+
+function ContainerStack() {
+  // const store = useSelector(store => store);
+  const isoke = true
+  // console.log('store', store.auth.user);
+  return (
+    <Stack.Navigator>
+      {!isoke ? (
+        <>
+          <Stack.Screen name="SignIn" component={LoginScreen} />
+          {/* <Stack.Screen name="SignUp" component={SignUpScreen} /> */}
+          {/* <Stack.Screen name="ResetPassword" component={ResetPassword} /> */}
+        </>
+      ) :
+        (
+          <>
+            <Stack.Screen
+              name="Home"
+              component={TabHome}
+            // options={{ headerShown: false }}
+            />
+            {/* <Stack.Screen name="Notifications" component={ProfileScreen} />
+            <Stack.Screen name="Profile" component={ProfileScreen} />
+            <Stack.Screen name="Setting" component={SettingScreen} /> */}
+          </>
+        )
+      }
+    </Stack.Navigator>
   );
 }
 
 function App() {
   return (
     <NavigationContainer>
-      <MyTabs />
+      <ContainerStack />
+      {/* <MyTabs /> */}
     </NavigationContainer>
   );
 }
